3026a0b9bfdfef4a5863e6174a939749b5a452a6
Dimitris Vavoulis <dimitris.vavoulis@gmail.com>
2016-06-09 09:30:44 +0100

    Further work on benchmarks

--- plot_metrics.R
+++ plot_metrics.R
@@ -28,25 +28,37 @@ metrics_flat = metrics %>%
 
 mdls = c('nbin4', 'pois', 'bin','log', 'bcox', 'blom', 'voom', 'vst')
 metric = 'M'
-metrics_filt = metrics_flat %>%
-  filter(METRIC == metric, MODEL %in% mdls, SIZE == 8, NOISE == 1) %>%
+metrics_filt = metrics_flat %>% filter(METRIC == metric, MODEL %in% mdls, SIZE == 4, NOISE == 4)
+
+metrics_filt_summary = metrics_filt %>%
   group_by(METRIC, MODEL, NSAMPLES, SPARSITY, SIZE, NOISE) %>%
   arrange(desc(VALUE)) %>%
-  slice(1:3) %>%
+  # slice(1:3) %>%
+  summarise(MEAN = mean(VALUE), SD = sd(VALUE), SE = SD / sqrt(n())) %>%
   ungroup() %>%
   mutate(MODEL = factor(MODEL, levels = mdls, ordered = T))
 
 means = metrics_filt %>%
   group_by(METRIC, NSAMPLES, SPARSITY, SIZE, NOISE) %>%
-  summarise(MEAN = mean(VALUE), SD = sd(VALUE)) %>%
+  summarise(MEAN = mean(VALUE)) %>%
   ungroup()
+#
+# print(
+#   ggplot(data = metrics_filt, mapping = aes(x = MODEL, y = VALUE, fill = MODEL)) +
+#     geom_boxplot(alpha = 0.5, width = 0.5) +
+#     # geom_hline(data = means, aes(yintercept = MEAN), linetype = 'dashed') +
+#     # geom_hline(data = means, aes(yintercept = MEAN + SE), linetype = 'dashed', color = 'red') +
+#     # geom_hline(data = means, aes(yintercept = ifelse(MEAN - SE < 0, 0, MEAN - SE)), linetype = 'dashed', color = 'red') +
+#     theme(legend.position = 'none', axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+#     scale_x_discrete(name = '') +
+#     scale_y_continuous(name = metric) +
+#     facet_grid(SPARSITY ~ NSAMPLES)
+# )
 
 print(
-  ggplot(data = metrics_filt, mapping = aes(x = MODEL, y = VALUE, fill = MODEL)) +
-    geom_boxplot(alpha = 0.5, width = 0.5) +
-    # geom_hline(data = means, aes(yintercept = MEAN), linetype = 'dashed') +
-    # geom_hline(data = means, aes(yintercept = MAX), linetype = 'dashed', color = 'red') +
-    # geom_hline(data = means, aes(yintercept = MEAN - SD), linetype = 'dashed') +
+  ggplot(data = metrics_filt_summary, mapping = aes(x = MODEL, y = MEAN, color = MODEL)) +
+    geom_pointrange(aes(ymax = MEAN + SE, ymin = MEAN - SE), size = 0.25, shape = 0) +
+    geom_hline(data = means, aes(yintercept = MEAN), linetype = 'dashed', size = 0.25) +
     theme(legend.position = 'none', axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
     scale_x_discrete(name = '') +
     scale_y_continuous(name = metric) +
