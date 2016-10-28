1f15b60f90296dfbc458e374b43614abd125ac9b
Dimitris Vavoulis <dimitris.vavoulis@gmail.com>
2016-10-28 16:15:56 +0100

    Code refactoring

--- eQTLseq/driver.py
+++ eQTLseq/driver.py
@@ -1,7 +1,5 @@
 """Implements run()."""
 
-import multiprocessing as _mlp
-
 import numpy as _nmp
 import tqdm as _tqdm
 
@@ -13,67 +11,73 @@ from eQTLseq.ModelNormalGibbs import ModelNormalGibbs as _ModelNormalGibbs
 from eQTLseq.ModelPoissonGibbs import ModelPoissonGibbs as _ModelPoissonGibbs
 
 
-def run(Z, G, mdl='Normal', scale=True, n_iters=1000, burnin=0.5, beta_thr=1e-6, s2_lims=(1e-20, 1e3), n_threads=1,
-        progress=True, **extra):
+def run(Z, G, model='Normal', scale=True, n_iters=1000, burnin=0.5, beta_thr=1e-6, s2_lims=(1e-20, 1e3), n_threads=1,
+        progress=True):
     """Run an estimation algorithm for a specified number of iterations."""
     Z = Z.T
-    n_threads = _mlp.cpu_count() if n_threads is None else n_threads
-    n_burnin = round(n_iters * burnin)
-    assert mdl in ('Normal', 'Poisson', 'Binomial', 'NBinomial')
-
     n_samples1, n_genes = Z.shape
     n_samples2, n_markers = G.shape
 
     assert n_samples1 == n_samples2
+    assert model in ('Normal', 'Poisson', 'Binomial', 'NBinomial')
+    assert n_iters > 0
+    assert 0.5 <= burnin < 1
+    assert 0 < beta_thr < 1e-6
 
-    # arguments
+    # data
     G = (G - _nmp.mean(G, 0)) / _nmp.std(G, 0)
     GTG = G.T.dot(G)
 
+    if model == 'Normal':
+        Y = (Z - _nmp.mean(Z, 0)) / _nmp.std(Z, 0) if scale else Z - _nmp.mean(Z, 0)
+        YTY = _nmp.sum(Y**2, 0)
+        GTY = G.T.dot(Y)
+    else:
+        Y = YTY = GTY = None
+
+    # args
     args = {
+        'model': model,
         'n_samples': n_samples1,
         'n_markers': n_markers,
         'n_genes': n_genes,
         'n_iters': n_iters,
-        'n_burnin': n_burnin,
+        'n_burnin': round(n_iters * burnin),
         'beta_thr': beta_thr,
         's2_lims': s2_lims,
         'scale': scale,
         'Z': Z,
+        'Y': Y,
         'G': G,
         'GTG': GTG,
-        **extra
+        'YTY': YTY,
+        'GTY': GTY
     }
 
-    if mdl == 'Normal':
-        args['Y'] = (Z - _nmp.mean(Z, 0)) / _nmp.std(Z, 0) if scale else Z - _nmp.mean(Z, 0)
-        args['YTY'] = _nmp.sum(args['Y']**2, 0)
-        args['GTY'] = G.T.dot(args['Y'])
-
-    # prepare model
-    Model = {
-        'Poisson': _ModelPoissonGibbs,
-        'Binomial': _ModelBinomGibbs,
-        'NBinomial': _ModelNBinomGibbs,
-        'Normal': _ModelNormalGibbs,
-    }[mdl]
-    mdl = Model(**args)
-
     # loop
-    state = _nmp.empty(n_iters + 1)
-    state.fill(_nmp.nan)
-    state[0] = 0
-    _prl.Pool = None if n_threads <= 1 else _mlp.Pool(processes=n_threads)
+    _prl.init(n_threads)
+    mdl = _generate_model(**args)
+    state = _nmp.r_[0, [_nmp.nan] * n_iters]
     for itr in _tqdm.tqdm(range(1, n_iters + 1), disable=not progress):
         mdl.update(itr, **args)
         state[itr] = mdl.get_state(**args)
-
-    if _prl.Pool is not None:
-        _prl.Pool.close()
-        _prl.Pool.join()
+    _prl.clean()
 
     #
     return {
         'state': state,
         **mdl.get_estimates(**args)
     }
+
+
+def _generate_model(**args):
+    """TODO."""
+    Model = {
+        'Poisson': _ModelPoissonGibbs,
+        'Binomial': _ModelBinomGibbs,
+        'NBinomial': _ModelNBinomGibbs,
+        'Normal': _ModelNormalGibbs,
+    }[args['model']]
+
+    ##
+    return Model(**args)
--- eQTLseq/parallel.py
+++ eQTLseq/parallel.py
@@ -1,3 +1,23 @@
 """Pool of workers for parallel processing."""
 
-Pool = None
+import multiprocessing as _mlp
+
+POOL = None
+
+
+def init(n_threads):
+    """TODO."""
+    global POOL
+    if n_threads == 1:
+        POOL = None
+    elif n_threads is None or n_threads < 1:
+        POOL = _mlp.Pool(processes=_mlp.cpu_count())
+    else:
+        POOL = _mlp.Pool(processes=n_threads)
+
+
+def clean():
+    """TODO."""
+    if POOL is not None:
+        POOL.close()
+        POOL.join()
