67f8371b8028ae62e2663233241cc54646d371bf
Dimitris <dimitris.vavoulis@gmail.com>
2017-02-09 11:40:14 +0000

    Added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='eQTLseq',
+    version='17.02.14',
+    description='Hierarchical probabilistic models for multiple gene/variant associations based on NGS data',
+    long_description=' ',
+    author='Dimitrios V. Vavoulis',
+    author_email='Dimitris.Vavoulis@ndcls.well.ox.ac.uk',
+    url='https://github.com/dvav/eQTLseq',
+    license='MIT',
+    platforms=['UNIX'],
+    install_requires=['numpy>=1.12.0', 'scipy>=0.18.1', 'rpy2>=2.8.5', 'tqdm>=4.11.2'],
+    packages=['eQTLseq'],
+    keywords='eQTL Bayesian RNA-seq',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ]
+)
