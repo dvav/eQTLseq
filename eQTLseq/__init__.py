4a084f5b0647fe6278c43d23d67d5d730fe2afa1
Dimitris <dimitris.vavoulis@gmail.com>
2017-02-09 15:44:29 +0000

    Updated __init__.py

--- eQTLseq/__init__.py
+++ eQTLseq/__init__.py
@@ -4,7 +4,6 @@ from eQTLseq.driver import run
 from eQTLseq.driver import get_metrics
 
 from eQTLseq.utils import calculate_metrics
-from eQTLseq.utils import get_pvalues
 
 from eQTLseq.sim import fit_nbinom_model
 from eQTLseq.sim import simulate_eQTLs
